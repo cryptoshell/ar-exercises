4591416b679446dcb1d4577582c36803212ab781
Yelim Oh <yelim.oh1@gmail.com>
2017-08-02 01:58:25 +0000

    Create more employees under store 1 and 2 in exercise_2

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,10 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+# Create more employees under store 1 and 2
+@store1.employees.create(first_name: "Yelim", last_name: "Oh", hourly_rate: 60)
+@store1.employees.create(first_name: "Bryce", last_name: "Loo", hourly_rate: 10)
+@store1.employees.create(first_name: "Raj", last_name: "Ha", hourly_rate: 50)
+@store2.employees.create(first_name: "Kai", last_name: "So", hourly_rate: 10)
+@store2.employees.create(first_name: "Gong", last_name: "Yoo", hourly_rate: 600)
+@store2.employees.create(first_name: "Dan", last_name: "Sho", hourly_rate: 6)
