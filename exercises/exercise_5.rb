5e16d1e6b5c12e87606d6e696dfef449b6faeedc
Yelim Oh <yelim.oh1@gmail.com>
2017-08-02 01:43:11 +0000

    Add calculation methods - sum, avg, count to exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,14 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+# Use sum to output total revenue
+@sum_revenue = Store.sum(:annual_revenue)
+puts @sum_revenue
+
+# Use average to output average revenue
+@avg_revenue = Store.average(:annual_revenue)
+puts @avg_revenue
+
+# Output num of stores that are generating $1M or more in annual sales
+@num_of_rich_stores = Store.where("annual_revenue > ?", 1000000).size
+puts @num_of_rich_stores
