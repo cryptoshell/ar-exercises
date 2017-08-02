a31a11f3ab9365244960128cd8dc5989921989fa
Yelim Oh <yelim.oh1@gmail.com>
2017-08-02 01:02:52 +0000

    Assign store 1 and 2 to instance variables and update store 1 name

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,11 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+# Assign store 1 and 2 to instance variables
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+# Update the first store with a new name
+@store1.name = "Yelim's store"
+p @store1.name
+
